e5a17c77b1576a9088041499e7a7cdbe2be52cb2
chourasiahemanshu <chourasiahemanshu@gmail.com>
2019-02-20 20:52:34 -0500

    Game Driver code/ just the first cut and basic functions

---
+++ RiskGame/src/GameDriver.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author Hemanshu
+ * @date 2019-02-20
+ */
+public class GameDriver {
+    static int playerNumber;
+    static ArrayList<Player> PlayerList = new ArrayList<Player>();
+
+
+    public static void InitialisePlayers(){
+        System.out.println("Number of Players who want to play :");
+        Scanner input = new Scanner(System.in);
+        playerNumber = input.nextInt();
+
+
+        for(int i = 0; i< playerNumber; i++){
+            System.out.println("Player "+(i+1)+" name :");
+            String name = input.next();
+            PlayerList.add(new Player(name));
+        }
+
+
+    }
+
+    public static void StartOrLoadGame(){
+        System.out.println("Select 1 to Start the game and create map, Select 2 to Load the Game :");
+        int localvariable;
+        Scanner input1 = new Scanner(System.in);
+        localvariable = input1.nextInt();
+        if(localvariable == 1){
+
+        } else if (localvariable == 2) {
+
+        }else{
+            System.out.println("Please enter Relevant option");
+        }
+    }
+
+
+    
+
+
+    public static void main(String[] args) {
+        InitialisePlayers();
+        StartOrLoadGame();
+    }
+}
