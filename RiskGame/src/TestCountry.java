8753cc594af69e14a81b77ff474a1d392679d2f3
vik-sandhu <31061931+vik-sandhu@users.noreply.github.com>
2019-03-02 04:49:13 -0500

    Update TestCountry.java

--- RiskGame/src/TestCountry.java
+++ RiskGame/src/TestCountry.java
@@ -54,3 +54,61 @@ public class TestCountry {
         assertEquals("India",name);
 
     }
+      @Test
+    public void TestContinentName()
+    {
+        String name = country.getParentContinent();
+        System.out.print(name);
+        assertEquals("Asia",name);
+
+    }
+
+
+    /**
+     * Test case to tests x value is correct
+     */
+
+    @Test
+    public void TestGetY() {
+        System.out.println("testGetY");
+        int y = country.getyCoordinate();
+        System.out.println(y);
+        assertEquals(0, y);
+    }
+
+
+    @Test
+    public void TestgetId() {
+
+        int id = country.getId();
+        System.out.println(id);
+        assertEquals(1, id);
+    }
+    /**
+     * returning in int
+     */
+    @Test
+    public void TestNumberOfSoldiers() {
+        System.out.println("NumberOfSoldiers");
+        int army = country.getNumberOfSoldiers();
+        System.out.println(army);
+        assertEquals(0, army);
+    }
+
+
+    /**
+     * Perform post-test clean-up.
+     *
+     * @throws Exception
+     *             if the clean-up fails for some reason
+     */
+
+    @After
+    public void TearDown() throws Exception {
+        System.out.println("");
+        country = null;
+        assertNull(country);
+    }
+}
+
+
