837d51bc33f26db884d3342b209fcca5760c8dce
VIKRAM SANDHU <vikramsandhu008@gmail.com>
2019-03-05 23:11:04 -0500

    test files

---
+++ RiskGame/Testing/TestPlayer.java
@@ -0,0 +1,39 @@
+import Model.Player;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/*
+
+ * @author Vikram
+ * @version 1.0.0
+ */
+
+
+public class TestPlayer {
+
+     @Before
+     public void BeforeTestCase()
+     {
+
+         Player player=new Player(1,"Vikram");
+     }
+    @Test
+    public void TestCase1()
+    {
+
+      int id=Player.getPlayerId();
+      assertEquals(1,id);
+
+    }
+
+    @Test
+    public void TestCase2()
+    {
+
+        String name=Player.getName();
+        assertEquals("Vikram",name);
+
+    }
+}
