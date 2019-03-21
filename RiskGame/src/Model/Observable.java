eaa6d95227138b0550decc7df26034c5c5579c13
soen6441project <jerint04@gmail.com>
2019-03-21 14:00:01 -0400

    Observer Class

---
+++ RiskGame/src/Model/Observable.java
@@ -0,0 +1,8 @@
+package Model;
+
+public interface Observable {
+
+    public void registerObserver(Observer observer);
+    public void notifyObservers();
+    public void removeObserver(Observer observer);
+}
