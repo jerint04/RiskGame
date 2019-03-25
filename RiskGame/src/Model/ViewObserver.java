f4c2ac3eaf132e358cfc6c075b597f7bc0920115
soen6441project <jerint04@gmail.com>
2019-03-25 16:14:16 -0400

    reverting back changes

--- RiskGame/src/Model/ViewObserver.java
+++ RiskGame/src/Model/ViewObserver.java
@@ -1,38 +1,41 @@
 package Model;
 
-import View.DisplayGuiHelp;
+import Controller.GameController;
 
 import java.util.Observable;
 import java.util.Observer;
 
-/**
- * VieObserver class
- * Initiate the risk game view in java swings
- *
- * @author Jerin
- * @version 1.0.0
- */
 public class ViewObserver implements Observer {
     String phaseView = "";
+    //DisplayGuiHelp gui1=new DisplayGuiHelp();
+
+
     @Override
-    /**
-     * Update method called by the observable object to perform all the actions
-     */
     public void update(Observable o, Object arg) {
+
         // TODO Auto-generated method stub
         //TurnOrganizer turnOrganizer = (TurnOrganizer) o;
+
         Player player=(Player) o;
-        if (((Player) o).getGamePhase() != "Initialisation")
+
+            if (((Player) o).getGamePhase() != "Initialisation")
         {
             phaseView = "Current Phase: " + player.getGamePhase() + "\n" + "Current Player: "
                     + player.getName();
-            DisplayGuiHelp gui1 = new DisplayGuiHelp(phaseView);
+            GameController.MssageOnGUI(phaseView);
+
+//            gui1.theText.setText(phaseView);
+//            gui1.dispose();
+
 
         }
-        else
-        {
+        else{
             phaseView = "Current Phase: " + player.getGamePhase();
-            DisplayGuiHelp gui1 = new DisplayGuiHelp(phaseView);
+//                gui1.theText.setText(phaseView);
+                GameController.MssageOnGUI(phaseView);
+
+
+
         }
 
 
@@ -42,9 +45,6 @@ public class ViewObserver implements Observer {
         System.out.println("_____End of Phase View_____");
     }
 
-    /**
-     * This method gets the view of the current phase
-     */
     public void GetPhaseView() {
         System.out.println(phaseView);
     }
