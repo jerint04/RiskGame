71bb2913a52526b5c6df6e4755b64c27bb3a4094
VIKRAM SANDHU <vikramsandhu008@gmail.com>
2019-04-09 23:05:33 -0400

    test helper commit

---
+++ RiskGame/Testing/TestTournamentMode.java
@@ -0,0 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static Controller.GameController.tournamentLoadMap;
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * This class tests the tournament mode.
+ *
+ * @author Vikram
+ * @version 1.0.0
+ */
+
+public class TestTournamentMode {
+
+    @Before
+    public void before()
+    {
+
+
+    }
+
+    @Test
+    public void TestCase1()
+    {
+
+     tournamentLoadMap("Asia");
+
+
+    }
+
+}
